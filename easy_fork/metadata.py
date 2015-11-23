655d2d3bae2b4d1e7951180765e2cfb171134def
huntzhan <programmer.zhx@gmail.com>
2015-11-23 11:29:16 +0800

    init.

---
+++ easy_fork/metadata.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+NAME = 'easy_fork'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'A tool to fork projects from github to gitlab.'
